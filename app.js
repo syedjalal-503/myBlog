27210b7df3c972b5b7621cfb66de226d86db5e63
syed jalal <syedjalaluddin48@gmail.com>
2026-03-29 17:53:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+import express from "express";
+import bodyparser from "body-parser";
+
+const app = express();
+const port = 3000;
+let posts= [];
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/",(req,res)=>
+{
+    res.render("index",{posts:posts});
+});
+
+app.get("/delete/:id",(req,res)=>{
+    const id=req.params.id;
+    posts=posts.filter(p => p.id!=id);
+    res.redirect("/");
+});
+app.get("/edit/:id", (req, res) => {
+  const postId = Number(req.params.id);
+  const post = posts.find(p => p.id === postId);
+    console.log(post);
+  if (!post) {
+    return res.send("❌ Post not found");
+  }
+
+  res.render("edits", { post });
+});
+app.post("/edit/:id", (req, res) => {
+  const postId = Number(req.params.id);
+     console.log(postId);
+  const postIndex = posts.findIndex(p => p.id === postId);
+
+  if (postIndex === -1) {
+    return res.send("Post not found");
+  }
+
+  // update post
+  posts[postIndex].title = req.body.title;
+  posts[postIndex].content = req.body.content;
+
+  res.redirect("/");
+});
+
+app.get("/new", (req, res) => {
+  res.render("new"); // render views/new.ejs
+   
+});
+
+app.post("/create",(req,res)=>
+{
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const newpost = {
+        id:Date.now(),
+        title:title,
+        content:content
+    }
+    console.log(newpost);
+   posts.push(newpost);
+    res.redirect("/");
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
